20101c5f512b49a90cac94ca7dc6bdc366dd5234
ata kaleli <atakaleli123@gmail.com>
2024-03-05 21:00:39 +0200

    ScoreAdded

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,34 +2,79 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
 
-
-    [SerializeField] private QuestionSO questionSO;
+    [Header("Questions")]
+     private QuestionSO currentQuestion;
     [SerializeField] private TextMeshProUGUI questionText;
-    [SerializeField] private GameObject[] answerButtons;
+    [SerializeField] List<QuestionSO> questions = new List<QuestionSO>();
 
+    [Header("Answers")]
+    [SerializeField] private GameObject[] answerButtons;
     private int correctAnswerIdx;
+    private bool hasAnsweredEarly;
+
+    [Header("Buttons")]
     [SerializeField] private Sprite defaultAnswerSprite;
     [SerializeField] private Sprite correctAnswerSprite;
 
+    [Header("Timer")]
+    [SerializeField] private Image timerImage;
+    private Timer timer;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private ScoreKeeper scoreKeeper;
+
+
     void Start()
     {
-        GetNextQuestion();
+        timer = FindObjectOfType<Timer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        
     }
 
-
+    private void Update()
+    {
+        timerImage.fillAmount = timer.fillFraction;
+        if (timer.loadNextQuestion)
+        {
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!hasAnsweredEarly && !timer.isAnsweringQuestion)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
+    }
 
     private void GetNextQuestion()
     {
-        SetButtonState(true);
-        DisplayQuestion();
-        SetDefaultButtonSprite();
+        if(questions.Count > 0)
+        {
+
+            SetButtonState(true);
+            SetDefaultButtonSprite();
+            GetRandomQuestion();
+            DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
+        }
         
+    }
+
+    private void GetRandomQuestion()
+    {
+        int index = UnityEngine.Random.Range(0, questions.Count);
+        currentQuestion = questions[index];
+
+        if(questions.Contains(currentQuestion))
+            questions.Remove(currentQuestion);
     }
 
     private void SetDefaultButtonSprite()
@@ -44,34 +89,43 @@ public class Quiz : MonoBehaviour
 
     private void DisplayQuestion()
     {
-        questionText.text = questionSO.GetQuestion();
+        questionText.text = currentQuestion.GetQuestion();
         for (int i = 0; i < answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = questionSO.GetAnswer(i);
+            buttonText.text = currentQuestion.GetAnswer(i);
 
         }
         
     }
 
-    public void OnAnswerSelected(int index)
+    void DisplayAnswer(int index)
     {
-        if(index == questionSO.GetCorrectAnswerIdx())
+        if (index == currentQuestion.GetCorrectAnswerIdx())
         {
             questionText.text = "Correct";
             Image buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
-            correctAnswerIdx = questionSO.GetCorrectAnswerIdx();
+            correctAnswerIdx = currentQuestion.GetCorrectAnswerIdx();
             int correctOption = correctAnswerIdx + 1;
             questionText.text = "False! Correct answer is option " + correctOption;
             Image buttonImage = answerButtons[correctAnswerIdx].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
 
         }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
         SetButtonState(false);
+        timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
 
